ea4a276a9584c47c97cb02bc89840a953dad3c1b
rakhat5555 <rakhat5555@gmail.com>
2019-03-02 02:25:34 -0800

    add arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
-var lists = ["some-input1","some-input2","some-input3"];
 
- lists.forEach(function(list){
-   var listInput = $("input#" + list).val();
+    var lists = [];
 
-   $("." + list).text(listInput.toUpperCase());
- });
+    var food = $("input#food").val();
+    var color = $("input#color").val();
+    var place= $("input#place").val();
 
-    // var person1Input = $("input#some-input1").val();
-    // var person2Input = $("input#some-input2").val();
-    // var animalInput= $("input#some-input3").val();
-    //
-    //
-    // $(".some-input1").text(person1Input);
-    // $(".some-input2").text(person2Input);
-    // $(".some-input3").text(animalInput);
+    var favList = [];
+
+    lists.push(food, color,place);
+   favList.push(lists[0], lists[1],lists[2]);
+
+    $(".food").text(lists[0]);
+    $(".color").text(lists[1]);
+    $(".place").text(lists[2]);
 
 
     $("#list").show();
